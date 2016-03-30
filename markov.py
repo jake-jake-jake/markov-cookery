9e8888a626ae496dad3e893cda13f6dd7ef65cda
Jake <jcdavis@awedge.net>
2016-03-30 18:01:52 -0400

    Add sentence() function

--- markov.py
+++ markov.py
@@ -2,6 +2,7 @@
 
 from os import path, listdir
 import random
+import string
 
 class successor_dict(dict):
     ''' Dictionary of successors following a word, with counts for weight.'''
@@ -13,7 +14,7 @@ class successor_dict(dict):
         self.out_list = []
 
     def _increment(self):
-        ''' Increment count and set self.weighted to False.''' 
+        ''' Increment count and set self.weighted to False.'''
         self.count += 1
         self.weighted = False
         return self
@@ -29,13 +30,14 @@ class successor_dict(dict):
         return self
 
     def choose_successor(self):
+        ''' Choose next word based on probabilities of input texts.'''
         if self.count == len(self.successors):
-            return random.choice([successor
-                                  for successor in self.successors])
+            return random.choice([successor for successor in self.successors])
         else:
             if not self.weighted:
                 self._make_weighted_out_list()
             return self.out_list[random.randrange(self.count)]
+
 
 class WordChainer:
     ''' Class for constructing a Markov chain from text files. '''
@@ -53,9 +55,10 @@ class WordChainer:
     @staticmethod
     def _find_successors(word_list):
         ''' Yield successor for each word in list.'''
+        marks = '\[]_/'
         for i, word in enumerate(word_list):
             try:
-                yield word, word_list[i+1]
+                yield word.strip(marks), word_list[i+1].strip(marks)
             except IndexError:
                 return
 
@@ -64,15 +67,15 @@ class WordChainer:
         if word not in self.links:
             self.links[word] = successor_dict()
             self.links[word][successor] = 1
-            self.links[word].count += 1
+            self.links[word]._increment()
             return self
         elif successor not in self.links[word]:
             self.links[word][successor] = 1
-            self.links[word].count += 1
+            self.links[word]._increment()
             return self
         else:
             self.links[word][successor] += 1
-            self.links[word].count += 1
+            self.links[word]._increment()
             return self
 
     def add_words(self, filename):
@@ -82,23 +85,49 @@ class WordChainer:
         for word, successor in successors:
             self._add_successor(word, successor)
 
-    def recipe(self, length, start_word=None):
+    def words(self, length, start_word=None):
         ''' Return words from Markov chain, beginning at optional start_word'''
         if not start_word:
             start_word = random.choice([word for word in self.links.keys()])
-        recipe = [start_word]
+        words = [start_word.capitalize()]
         try:
             next_word = self.links[start_word].choose_successor()
         except KeyError:
-            print('%s not in chain. Leave blank for random word.' % start_word)
+            print('start_word not in chain. Leave blank for random word.')
             return None
         for _ in range(length):
-            recipe.append(next_word)
+            words.append(next_word)
             next_word = self.links[next_word].choose_successor()
-        recipe = ' '.join(recipe)
-        if not recipe[-1] == ['.']:
-            recipe += '.'
-        return recipe
+        words = ' '.join(words)
+        if words[-1] in string.punctuation:
+            words = words[:-1]
+        return words + '.'
+
+    def sentence(self, length=None, start_word=None):
+        ''' Return a sentence of length, with start_word. '''
+        if not length:
+            length = random.randrange(12, 24)
+        if not start_word:
+            start_word = random.choice([word for word in self.links.keys()])
+        try:
+            next_word = self.links[start_word].choose_successor()
+        except KeyError:
+            print('start_word not in chain. Leave blank for random word.')
+            return None
+        sent = []
+        sent.append(start_word.capitalize())
+        for _ in range(length):
+            if next_word[-1] == '.' and next_word[:-1] in self.links:
+                next_word = next_word[:-1]
+            elif next_word[-1] == '.' and not next_word[:-1] in self.links:
+                print('shitbox')
+            sent.append(next_word)
+            next_word = self.links[next_word].choose_successor()
+        sent = ' '.join(sent)
+        if sent[-1] in string.punctuation:
+            sent = sent[:-1]
+        return sent + '.'
+
 
 chain = WordChainer()
 def main(debug=True):
