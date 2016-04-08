97031b1e2b28d8ed19ed685b6474aca3cedbeb5d
Jake <jcdavis@awedge.net>
2016-04-08 16:21:13 -0400

    Add bigram functionality; implement SuccessorDict as a Counter
    Full-scale refactoring. Added Counter as base class of SuccessorDict; also
    made the token a bigram rather than a single word. Yada yada.

--- wordchainer.py
+++ wordchainer.py
@@ -1,47 +1,32 @@
 #!/usr/bin/env python3
 
-from os import path, listdir
+from collections import Counter
+from os import path
 import random
 import string
 
-class successor_dict(dict):
+
+class SuccessorDict(Counter):
     ''' Dictionary of successors following a word, with counts for weight.'''
-
     def __init__(self):
-        self.count = 0
-        self.successors = self.keys()
-        self.weighted = False
+        self.count =- 0
         self.out_list = []
 
-    def _increment(self):
-        ''' Increment count and set self.weighted to False.'''
-        self.count += 1
-        self.weighted = False
-        return self
-
-    def _make_weighted_out_list(self):
+    def _make_out_list(self):
         ''' Generate list of successors.'''
-        self.weighted = True
-        self.out_list = []
-        for word, count in self.items():
-            for _ in range(count):
-                self.out_list.append(word)
-        assert len(self.out_list) == self.count
+        self.count = sum(self.values())
+        self.out_list = list(self.elements())
         return self
 
     def choose_successor(self):
         ''' Choose next word based on probabilities of input texts.'''
-        if self.count == len(self.successors):
-            return random.choice([successor for successor in self.successors])
-        else:
-            if not self.weighted:
-                self._make_weighted_out_list()
-            return self.out_list[random.randrange(self.count)]
+        if not self.out_list:
+            self._make_out_list()
+        return self.out_list[random.randrange(self.count)]
 
 
 class WordChainer:
     ''' Class for constructing a Markov chain from text files. '''
-
     def __init__(self):
         self.links = dict()
 
@@ -50,7 +35,7 @@ class WordChainer:
         ''' Open file, read it in entirely, split words, return that list.'''
         with open(filename, 'r') as f:
             content = f.read()
-        return content.split()  # Worry about preserving newline chars later.
+        return content.split()
 
     @staticmethod
     def _find_successors(word_list):
@@ -58,33 +43,24 @@ class WordChainer:
         marks = '\[]_/'
         for i, word in enumerate(word_list):
             try:
-                yield word.strip(marks), word_list[i+1].strip(marks)
+                yield (word, word_list[i+1]), word_list[i + 2]
             except IndexError:
                 return
 
-    def _add_successor(self, word, successor):
-        ''' Add word successors to self.links instance variable.'''
-        if word not in self.links:
-            self.links[word] = successor_dict()
-            self.links[word][successor] = 1
-            self.links[word]._increment()
-            return self
-        elif successor not in self.links[word]:
-            self.links[word][successor] = 1
-            self.links[word]._increment()
-            return self
-        else:
-            self.links[word][successor] += 1
-            self.links[word]._increment()
-            return self
+    def _add_successor(self, token, successor):
+        ''' Add token successors to self.links instance variable.'''
+        if token not in self.links:
+            self.links[token] = SuccessorDict()
+        self.links[token][successor] += 1
+        return self
 
-    def _get_start_word(self):
+    def _get_token(self):
         ''' Choose word at random; if it is a sentence ender, rechoose.'''
-        word = random.choice([word for word in self.links.keys()])
-        if not word[-1] == '.':
-            return word
+        token = random.choice([token for token in self.links.keys()])
+        if not token[0][-1] == '.':
+            return token
         else:
-            return self._get_start_word()
+            return self._get_token()
 
     def add_words(self, filename):
         ''' Make weighted Markov chain from text file.'''
@@ -93,15 +69,15 @@ class WordChainer:
         for word, successor in successors:
             self._add_successor(word, successor)
 
-    def words(self, length, start_word=None):
-        ''' Return words from Markov chain, beginning at optional start_word'''
-        if not start_word:
-            start_word = random.choice([word for word in self.links.keys()])
-        words = [start_word.capitalize()]
+    def words(self, length, token=None):
+        ''' Return words from Markov chain, beginning at optional token'''
+        if not token:
+            token = self._get_token()
+        words = [' '.join(token).capitalize()]
         try:
-            next_word = self.links[start_word].choose_successor()
+            next_word = self.links[token].choose_successor()
         except KeyError:
-            print('start_word not in chain. Leave blank for random word.')
+            print('token not in chain. Leave blank for random word.')
             return None
         for _ in range(length):
             words.append(next_word)
@@ -111,38 +87,32 @@ class WordChainer:
             words = words[:-1]
         return words + '.'
 
-    def sentence(self, start_word=None):
-        ''' Return a sentence of length, with start_word. '''
-        if not start_word:
-            start_word = self._get_start_word()
-        try:
-            next_word = self.links[start_word].choose_successor()
-        except KeyError:
-            print('start_word not in chain. Leave blank for random word.')
-            return None
-        sent = []
-        sent.append(start_word.capitalize())
+    def sentence(self, token=None):
+        ''' Return a sentence of length, with token. '''
+        if not token:
+            token = self._get_token()
+        sent = [' '.join(token).capitalize()]
+        next_word = self.links[token].choose_successor()
         while not next_word[-1] == '.':
+            token = token[1], next_word
             sent.append(next_word)
-            next_word = self.links[next_word].choose_successor()
+            next_word = self.links[token].choose_successor()
         else:
             sent.append(next_word)
         return ' '.join(sent)
 
 
 recipes = WordChainer()
-title = WordChainer()
+titles = WordChainer()
 
 def main(debug=True):
-    p = path.join('texts', '1600s')
-    recipes.add_words(path.join(p, 'accomplisht_cook_STRIPPED.txt'))
-    recipes.add_words(path.join(p, 'closet_of_sir_digby_STRIPPED.txt'))
-    recipes.add_words(path.join(p, 'eales_receipts_STRIPPED.txt'))
-    title.add_words(path.join(p, '1600s_titles.txt'))
+    if debug:
+        p = path.join('texts', '1600s')
+        recipes.add_words(path.join(p, 'accomplisht_cook_STRIPPED.txt'))
+        recipes.add_words(path.join(p, 'closet_of_sir_digby_STRIPPED.txt'))
+        recipes.add_words(path.join(p, 'eales_receipts_STRIPPED.txt'))
+        titles.add_words(path.join(p, '1600s_titles.txt'))
 
 
 if __name__ == '__main__':
     main()
-
-print(title.words(5))
-print(recipes.sentence())
