7b25d137ee6da969f1f84a87d0df7dea32f2e726
Jake <jcdavis@awedge.net>
2016-04-13 17:16:00 -0400

    Implement SQL database for saving recipes

--- cookery.py
+++ cookery.py
@@ -11,8 +11,8 @@ import flickrapi
 from wordchainer import WordChainer
 
 # Flask and database services
-from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
 
 # configuration
 # DATABASE = '/tmp/flaskr.db'
@@ -21,36 +21,12 @@ SECRET_KEY = 'development key'
 USERNAME = 'admin'
 PASSWORD = 'default'
 BRITISH_LIB_UID = '12403504@N02'
-
+SQLALCHEMY_DATABASE_URI = 'sqlite+pysqlite:///test.db'
 
 # CREATE ZEE APPLICATION
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-# Functions for initializing and connecting to database
-# def connect_db():
-#     return sqlite3.connect(app.config['DATABASE'])
-
-
-# def init_db():
-#     with closing(connect_db()) as db:
-#         with app.open_resource('schema.sql', mode='r') as f:
-#             db.cursor().executescript(f.read())
-#         db.commit()
-
-
-# # Request handling functions; `g` is a special Flask object.
-# @app.before_request
-# def before_request():
-#     g.db = connect_db()
-
-
-# @app.teardown_request
-# def teardown_request(exception):
-#     db = getattr(g, 'db', None)
-#     if db is not None:
-#         db.close()
-
+db = SQLAlchemy(app)
 
 @app.route('/')
 def make_recipe():
@@ -59,38 +35,20 @@ def make_recipe():
     pic = image_from_title(title)
     return render_template('make_recipe.html', pic=pic, title=title, recipe=recipe)
 
-
-# @app.route('/add', methods=['POST'])
-# def add_entry():
-#     if not session.get('logged_in'):
-#         abort(401)
-#     g.db.execute('insert into entries (title, text) values (?, ?)',
-#                  [request.form['title'], request.form['text']])
-#     g.db.commit()
-#     flash('New entry was successfully posted')
-#     return redirect(url_for('show_entries'))
+@app.route('/<int:id>')
+def saved_recipe(id):
+    recipe = db.session.query(Recipe).get(id)
+    return render_template('make_recipe.html', pic=recipe.img_url, 
+                           title=recipe.title, recipe=recipe.text)
 
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         if request.form['username'] != app.config['USERNAME']:
-#             error = 'Invalid username'
-#         elif request.form['password'] != app.config['PASSWORD']:
-#             error = 'Invalid password'
-#         else:
-#             session['logged_in'] = True
-#             flash('You were logged in')
-#             return redirect(url_for('show_entries'))
-#     return render_template('login.html', error=error)
+@app.route('/save', methods=['POST'])
+def save_recipe():
+    recipe = Recipe(img_url=request.form['img_url'], title=request.form['title'], text=request.form['recipe'])
+    db.session.add(recipe)
+    db.session.commit()
+    return redirect('/' + str(recipe.id))
 
-
-# @app.route('/logout')
-# def logout():
-#     session.pop('logged_in', None)
-#     flash('You were logged out')
-#     return redirect(url_for('show_entries'))
 
 
 # Flickr functions (for getting images)
@@ -142,6 +100,12 @@ def image_from_title(title):
     else:
         return 'DEFAULT IMAGE'
 
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    img_url = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    text = db.Column(db.String, nullable=False)
 
 
 
